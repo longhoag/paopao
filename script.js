fa0a0f8dee7670741c864b34c63b26d3ab607274
Ambermate <ambermate.tech@gmail.com>
2023-07-05 13:10:58 +0200

    typing

---
+++ script.js
@@ -0,0 +1,19 @@
+// const text = document.querySelector(".dynamic");
+
+// const textLoad = () => {
+//     setTimeout(() => {
+//         text.textContent = "Free";
+//     }, 0);
+
+//     setTimeout(() => {
+//         text.textContent = "Joker";
+//     }, 1000);
+
+//     setTimeout(() => {
+//         text.textContent = "Life";
+//     }, 2000);
+
+// }
+// textLoad();
+
+// setInterval(textLoad, 2000);
